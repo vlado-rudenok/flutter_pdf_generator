c7cbaaf04f81fb191a1caee6c4bfa8acec2efdfd
Volodymyr Rudenok <vlado.rudenok@gmail.com>
2024-08-03 18:10:00 +0200

    Android support

---
+++ android/src/main/kotlin/com/vlqdo/flutter_pdf_generator/FlutterPdfGeneratorPlugin+Extensions.kt
@@ -0,0 +1,57 @@
+package com.vlqdo.flutter_pdf_generator
+
+
+import android.annotation.SuppressLint
+import android.os.Handler
+import android.print.PdfPrint
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import io.flutter.plugin.common.MethodChannel.Result
+
+@SuppressLint("SetJavaScriptEnabled")
+fun FlutterPdfGeneratorPlugin.createWebView(content: String): WebView {
+    val webView = WebView(activity.applicationContext)
+    val width = this.activity.window.windowManager.defaultDisplay.width
+    val height = this.activity.window.windowManager.defaultDisplay.height
+    webView.layout(0, 0, width, height)
+    webView.loadDataWithBaseURL(null, content, "text/html", "UTF-8", null)
+    return webView
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+fun FlutterPdfGeneratorPlugin.configureWebView(webView: WebView) {
+    with(webView.settings) {
+        javaScriptEnabled = true
+        useWideViewPort = true
+        javaScriptCanOpenWindowsAutomatically = true
+        loadWithOverviewMode = true
+    }
+    WebView.enableSlowWholeDocumentDraw()
+}
+
+fun FlutterPdfGeneratorPlugin.setupWebViewClient(
+    webView: WebView,
+    savedPath: String?,
+    format: Map<String, Double>?,
+    footerText: String?,
+    headerText: String?,
+    duration: Double,
+    result: Result
+) {
+    webView.webViewClient = object : WebViewClient() {
+        override fun onPageFinished(view: WebView, url: String) {
+            super.onPageFinished(view, url)
+            Handler().postDelayed({
+                webView.exportAsPdfFromWebView(savedPath!!, format!!, footerText, headerText, object : PdfPrint.CallbackPrint {
+                    override fun success(path: String) {
+                        result.success(path)
+                    }
+
+                    override fun onFailure() {
+                        result.success(null)
+                    }
+                })
+            }, duration.toLong())
+        }
+    }
+}
